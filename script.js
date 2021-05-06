4c25393ade1234c964407f5ebbffec2780acda1e
navneet <navneetsran33@gmail.com>
2021-05-06 17:51:23 +0000

    neww.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-body {background-color:black;font-family:arial;color:white}
-h1{color:grey;font-family:verdana}
-.maindiv {
-    background-color:black;
-    color:white;
-    width:300px;
-    padding:20px;
-    border-radius:25px;
-    align:center
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
+

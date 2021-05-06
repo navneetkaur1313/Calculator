126e36221c447a69bbd661eefa5c821ec9872863
navneet <navneetsran33@gmail.com>
2021-05-06 17:36:41 +0000

    new.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+body {background-color:black;font-family:arial;color:white}
+h1{color:grey;font-family:verdana}
+.maindiv {
+    background-color:black;
+    color:white;
+    width:300px;
+    padding:20px;
+    border-radius:25px;
+    align:center
 }
-        
